9f4c65822be5e98667507541adf01595f7644208
diaganaamidou <diaganaamidou@gmail.com>
2026-09-22 23:02:48 +0000

    Initialisation

---
+++ src/main/java/com/psyavocat/PsyAvocatApplication.java
@@ -0,0 +1,13 @@
+package com.psyavocat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsyAvocatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PsyAvocatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/psyavocat/PsyAvocatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.psyavocat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PsyAvocatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
